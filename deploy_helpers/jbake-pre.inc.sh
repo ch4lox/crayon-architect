712aac3b70d1c8116f00a1ef5353f42b3d9c5139
James Cole <james@xrks.com>
2016-12-01 20:43:09 -0800

    changed _PATH to _DIR

--- deploy_helpers/jbake-pre.inc.sh
+++ deploy_helpers/jbake-pre.inc.sh
@@ -7,25 +7,25 @@ set -e
 # $CRAYON_ARCHITECT_DIR = the directory of the crayon-architect tools
 # $CRAYON_PROJECT_DIR = the directory to run the build tools in - uses pwd if undefined, this will be the working directory
 
-export DEPLOY_BUILD_OUTPUT_PATH=$(grep -Eo '^destination\.folder\s*=\s*.+' "jbake.properties"|cut -d '=' -f 2|xargs)
-if [ -z "${DEPLOY_BUILD_OUTPUT_PATH}" ]; then
-	export DEPLOY_BUILD_OUTPUT_PATH="./output"
+export DEPLOY_BUILD_OUTPUT_DIR=$(grep -Eo '^destination\.folder\s*=\s*.+' "jbake.properties"|cut -d '=' -f 2|xargs)
+if [ -z "${DEPLOY_BUILD_OUTPUT_DIR}" ]; then
+	export DEPLOY_BUILD_OUTPUT_DIR="./output"
 fi
-echo "DEPLOY_BUILD_OUTPUT_PATH is $DEPLOY_BUILD_OUTPUT_PATH"
+echo "DEPLOY_BUILD_OUTPUT_DIR is $DEPLOY_BUILD_OUTPUT_DIR"
 
-if [ ! -d "${DEPLOY_BUILD_OUTPUT_PATH}" ]; then
+if [ ! -d "${DEPLOY_BUILD_OUTPUT_DIR}" ]; then
 	export CRAYON_PRE_DEPLOY_SKIP="true";
 	export CRAYON_CMD_DEPLOY_SKIP="true";
 	
-	echo "${DEPLOY_BUILD_OUTPUT_PATH} does not exist!";
+	echo "${DEPLOY_BUILD_OUTPUT_DIR} does not exist!";
 fi
 
 if [ "$CRAYON_PRE_DEPLOY_SKIP" != "true" ]; then
-	if [ ! -f "${DEPLOY_BUILD_OUTPUT_PATH}/index.html" ]; then
+	if [ ! -f "${DEPLOY_BUILD_OUTPUT_DIR}/index.html" ]; then
 		export CRAYON_PRE_DEPLOY_SKIP="true";
 		export CRAYON_CMD_DEPLOY_SKIP="true";
 	
-		echo "${DEPLOY_BUILD_OUTPUT_PATH}/index.html does not exist!";
+		echo "${DEPLOY_BUILD_OUTPUT_DIR}/index.html does not exist!";
 	fi
 fi
 
